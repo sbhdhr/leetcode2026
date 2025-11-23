09020851a7f5311fabfb1ac3ea8e92d924e17b1c
Subhashis Dhar <sbhdhr@gmail.com>
2025-11-23 19:21:57 +0530

    Added Q1

--- Leetcode2026/Leetcode2026/Neetcode150/Question1.cs
+++ Leetcode2026/Leetcode2026/Neetcode150/Question1.cs
@@ -1,11 +1,47 @@
 ﻿namespace Leetcode2026.Neetcode150
 {
-    /* Question 1
-     * 
-     * Description:
-     * Implement the solution for Question 1 here.
-     * 
+    /* 217. Contains Duplicate
+        Easy
+
+        Given an integer array nums, return true if any value appears at least twice 
+        in the array, and return false if every element is distinct.
+
+ 
+
+        Example 1:
+
+        Input: nums = [1,2,3,1]
+
+        Output: true
+
+        Explanation:
+
+        The element 1 occurs at the indices 0 and 3.
+
+        Example 2:
+
+        Input: nums = [1,2,3,4]
+
+        Output: false
+
+        Explanation:
+
+        All elements are distinct.
+
+        Example 3:
+
+        Input: nums = [1,1,1,3,3,4,3,2,4,2]
+
+        Output: true
+
+ 
+
+        Constraints:
+
+        1 <= nums.length <= 10^5
+        -10^9 <= nums[i] <= 10^9
      */
+
     public static class Question1
     {
         // Helper methods
@@ -14,7 +50,15 @@
         //Solution method
         public static bool ContainsDuplicate(List<int> nums)
         {
-            
+            HashSet<int> seen = [];
+            foreach (int num in nums)
+            {
+                if (seen.Contains(num))
+                {
+                    return true;
+                }
+                seen.Add(num);
+            }
             return false;
         }
 
