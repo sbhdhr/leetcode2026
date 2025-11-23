8097194b8f61d9831fe688e387831bcdd6eca071
sbhdhr <39555321+sbhdhr@users.noreply.github.com>
2025-11-23 18:56:42 +0530

    Initial commit. Organised files

---
+++ Leetcode2026/Leetcode2026/Neetcode150/Question1.cs
@@ -0,0 +1,39 @@
+﻿namespace Leetcode2026.Neetcode150
+{
+    /* Question 1
+     * 
+     * Description:
+     * Implement the solution for Question 1 here.
+     * 
+     */
+    public static class Question1
+    {
+        // Helper methods
+
+
+        //Solution method
+        public static bool ContainsDuplicate(List<int> nums)
+        {
+            
+            return false;
+        }
+
+        public static void Run()
+        {
+            // Create test cases
+            List<int> nums = [ 1, 2, 3, 1 ];
+
+
+
+            // Execute the method
+            if (ContainsDuplicate(nums))
+            {
+                Console.WriteLine("Contains duplicate");
+            }
+            else
+            {
+                Console.WriteLine("No duplicates");
+            }
+        }
+    }
+}
---
+++ Leetcode2026/Leetcode2026/Program.cs
@@ -0,0 +1,5 @@
+﻿// Main driver program for Leetcode2026 solution
+using Leetcode2026.Neetcode150;
+
+Question1.Run();
+//Question2.Run();
