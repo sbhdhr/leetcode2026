40f3a97fbd6ca5c62e4682559b7e52d8f4935b4e
Subhashis Dhar <sbhdhr@gmail.com>
2025-12-01 12:00:20 +0530

    Organised code

--- Leetcode2026/Neetcode150/Question001.cs
+++ Leetcode2026/Neetcode150/Question001.cs
@@ -1,83 +1,84 @@
-﻿namespace Leetcode2026.Neetcode150
+﻿namespace Leetcode2026.Neetcode150;
+
+/*
+  217. Contains Duplicate
+  Easy
+
+  Given an integer array nums, return true if any value appears at least twice 
+  in the array, and return false if every element is distinct.
+
+
+
+  Example 1:
+
+  Input: nums = [1,2,3,1]
+
+  Output: true
+
+  Explanation:
+
+  The element 1 occurs at the indices 0 and 3.
+
+  Example 2:
+
+  Input: nums = [1,2,3,4]
+
+  Output: false
+
+  Explanation:
+
+  All elements are distinct.
+
+  Example 3:
+
+  Input: nums = [1,1,1,3,3,4,3,2,4,2]
+
+  Output: true
+
+
+
+  Constraints:
+
+  1 <= nums.length <= 10^5
+  -10^9 <= nums[i] <= 10^9
+*/
+
+public static class Question001
 {
-    /* 217. Contains Duplicate
-        Easy
+    // Helper methods
 
-        Given an integer array nums, return true if any value appears at least twice 
-        in the array, and return false if every element is distinct.
 
- 
-
-        Example 1:
-
-        Input: nums = [1,2,3,1]
-
-        Output: true
-
-        Explanation:
-
-        The element 1 occurs at the indices 0 and 3.
-
-        Example 2:
-
-        Input: nums = [1,2,3,4]
-
-        Output: false
-
-        Explanation:
-
-        All elements are distinct.
-
-        Example 3:
-
-        Input: nums = [1,1,1,3,3,4,3,2,4,2]
-
-        Output: true
-
- 
-
-        Constraints:
-
-        1 <= nums.length <= 10^5
-        -10^9 <= nums[i] <= 10^9
-     */
-
-    public static class Question001
+    //Solution method
+    public static bool ContainsDuplicate(List<int> nums)
     {
-        // Helper methods
-
-
-        //Solution method
-        public static bool ContainsDuplicate(List<int> nums)
+        HashSet<int> seen = [];
+        foreach (int num in nums)
         {
-            HashSet<int> seen = [];
-            foreach (int num in nums)
+            if (seen.Contains(num))
             {
-                if (seen.Contains(num))
-                {
-                    return true;
-                }
-                seen.Add(num);
+                return true;
             }
-            return false;
+            seen.Add(num);
         }
+        return false;
+    }
 
-        public static void Run()
+    public static void Run()
+    {
+        // Create test cases
+        List<int> nums = [1, 2, 3, 1];
+
+
+
+        // Execute the method
+        if (ContainsDuplicate(nums))
         {
-            // Create test cases
-            List<int> nums = [ 1, 2, 3, 1 ];
-
-
-
-            // Execute the method
-            if (ContainsDuplicate(nums))
-            {
-                Console.WriteLine("Contains duplicate");
-            }
-            else
-            {
-                Console.WriteLine("No duplicates");
-            }
+            Console.WriteLine("Contains duplicate");
+        }
+        else
+        {
+            Console.WriteLine("No duplicates");
         }
     }
 }
+
---
+++ Leetcode2026/Neetcode150/Question002.cs
@@ -0,0 +1,23 @@
+﻿namespace Leetcode2026.Neetcode150;
+
+/*
+  
+*/
+
+public static class Question002
+{
+    // Helper methods
+
+
+    //Solution method
+
+    public static void Run()
+    {
+        // Create test cases
+
+
+
+        // Execute the method
+    }
+}
+
